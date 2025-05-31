f5f2abdd5eb3eeb56272fb4267a9c356d920bda0
sarthu210 <sarthu102@gmail.com>
2025-05-31 16:01:58 +0530

    Project Intialized

---
+++ realestatecrm/src/main/java/com/crm/realestatecrm/RealestatecrmApplication.java
@@ -0,0 +1,13 @@
+package com.crm.realestatecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestatecrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestatecrmApplication.class, args);
+	}
+
+}
